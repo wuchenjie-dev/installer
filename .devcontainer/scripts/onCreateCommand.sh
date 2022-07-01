311233478afef03aca3d60da762169cc7fe0b6e2
Logan Bussell <loganbussell@microsoft.com>
2022-07-01 15:12:39 -0700

    Try something simpler

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -e pipefail
 
 ./build.sh /p:ArcadeBuildTarball=true /p:TarballDir=/workspaces/dotnet-source/ /p:PreserveTarballGitFolders=true
 
-cd /workspaces/dotnet-source/
+# cd /workspaces/dotnet-source/
 
 # save the commit hash of the currently built repo, so developers know which version was built
 git rev-parse HEAD > ./artifacts/prebuild.sha
