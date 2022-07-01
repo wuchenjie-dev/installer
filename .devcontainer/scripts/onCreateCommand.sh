124a7b6309e1e47a63fa357435229562721e8e79
Logan Bussell <loganbussell@microsoft.com>
2022-07-01 23:29:33 +0000

    Build dotnet in the codespaces prebuild

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -4,7 +4,10 @@ set -e pipefail
 
 ./build.sh /p:ArcadeBuildTarball=true /p:TarballDir=/workspaces/dotnet-source/ /p:PreserveTarballGitFolders=true
 
-# cd /workspaces/dotnet-source/
+cd /workspaces/dotnet-source/
+
+./prep.sh
+./build.sh --online
 
 # save the commit hash of the currently built repo, so developers know which version was built
 git rev-parse HEAD > ./artifacts/prebuild.sha
