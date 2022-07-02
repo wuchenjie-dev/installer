796fcdb279f0e3a5362e6b3c17cce99e6d79d6d5
Logan Bussell <loganbussell@microsoft.com>
2022-07-02 00:01:53 +0000

    Always return exit code 0 from failure-prone commands

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -2,12 +2,13 @@
 
 set -e pipefail
 
-./build.sh /p:ArcadeBuildTarball=true /p:TarballDir=/workspaces/dotnet-source/ /p:PreserveTarballGitFolders=true
+# We want to always have exit code 0 so that we can use the codespace prebuild to diagnose errors.
+./build.sh /p:ArcadeBuildTarball=true /p:TarballDir=/workspaces/dotnet-source/ /p:PreserveTarballGitFolders=true || true
 
 cd /workspaces/dotnet-source/
 
-./prep.sh
-./build.sh --online
+./prep.sh || true
+./build.sh --online || true
 
 # save the commit hash of the currently built repo, so developers know which version was built
 git rev-parse HEAD > ./artifacts/prebuild.sha
