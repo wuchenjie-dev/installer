7fb62ea951a583890fa066419dd069d3bb52a8c8
Logan Bussell <loganbussell@microsoft.com>
2022-07-05 22:14:04 +0000

    Don't run git in a non-git directory

--- .devcontainer/scripts/tarballBuild.sh
+++ .devcontainer/scripts/tarballBuild.sh
@@ -3,10 +3,10 @@
 # We want to always have exit code 0 so that we can use the codespace prebuild to diagnose errors.
 ./build.sh /p:ArcadeBuildTarball=true /p:TarballDir=/workspaces/dotnet-source/ /p:PreserveTarballGitFolders=true || true
 
+# save the commit hash of the currently built repo, so developers know which version was built
+git rev-parse HEAD > ./artifacts/prebuild.sha
+
 cd /workspaces/dotnet-source/
 
 ./prep.sh
 ./build.sh --online || true
-
-# save the commit hash of the currently built repo, so developers know which version was built
-git rev-parse HEAD > ./artifacts/prebuild.sha
