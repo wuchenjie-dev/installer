fe4197e60e6465f1a674cfe67df0ceb68e4aba4d
Logan Bussell <loganbussell@microsoft.com>
2022-07-05 10:40:11 -0700

    Add codespaces configuration for only tarball creation

---
+++ .devcontainer/scripts/tarballCreate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# We want to always have exit code 0 so that we can use the codespace prebuild to diagnose errors.
+./build.sh /p:ArcadeBuildTarball=true /p:TarballDir=/workspaces/dotnet-source/ /p:PreserveTarballGitFolders=true || true
+
+# save the commit hash of the currently built repo, so developers know which version was built
+git rev-parse HEAD > ./artifacts/prebuild.sha
